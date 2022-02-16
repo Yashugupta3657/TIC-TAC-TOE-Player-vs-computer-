ed489e4203e916b738ce44f00745ae29df652a5a
Yashugupta3657 <60153103+Yashugupta3657@users.noreply.github.com>
2022-02-16 17:06:55 +0530

    Update Game.c

--- Game.c
+++ Game.c
@@ -7,220 +7,348 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
+
 #include <stdlib.h>
+
+#include <string.h>
+
 #include <time.h>
-void
-green ()
-{
-  printf ("\033[2;32m");
-}				// FOR GIVING COLOURING EFFECT TO CODE // 
 
 void
-blue ()
-{
-  printf ("\033[2;34m");
-}
-
+green() {
+        printf("\033[2;32m");
+} // FOR GIVING COLOURING EFFECT TO CODE // 
 
 void
-yellow ()
-{
-  printf ("\033[2;33m");
+blue() {
+        printf("\033[2;34m");
 }
 
 void
-reset ()
-{
-  printf ("\033[0m");
+yellow() {
+        printf("\033[2;33m");
 }
 
-char matrix[3][3];		// DEFINING MATRIX //
+void
+reset() {
+        printf("\033[0m");
+}
 
-char Result (void);		// CHECKING IF SOMEONE WON OR LOOSE OR GAME IS DRAW //
+char matrix[3][3]; // DEFINING MATRIX //
 
-void Initializing (void);	// INITIALIZING VALUES TO THE MATRIX //
+char Result(void); // CHECKING IF SOMEONE WON OR LOOSE OR GAME IS DRAW //
 
-void Playermove (void);		// INPUTS PLAYER MOVE //
+void Initializing(void); // INITIALIZING VALUES TO THE MATRIX //
 
-void Computermove (void);	// INPUTS COMPUTER MOVE //
+void Playermove(void); // INPUTS PLAYER MOVE //
 
-void Show_matrix (void);	// FOR DISPLAYING REFRESHED MATRIX AFTER EACH MOVE //
+void Computermove(void); // INPUTS COMPUTER MOVE //
 
-int
-main (void)
-{
+void Show_matrix(void); // FOR DISPLAYING REFRESHED MATRIX AFTER EACH MOVE //
 
-  char done;
-  blue ();
-  printf ("GAME OF TIC TAC TOE:\n");
+void runU(void);
 
-  printf ("PLAYER WILL BE PLAYING AGAINST COMPUTER...\n");
+void check(void);
 
-  done = ' ';
+int generate_random(int l,int r);
 
-  Initializing ();
+void call(void);
 
-  while (done == ' ')
-    {
+void runC(void);
 
-      Show_matrix ();
+void Playermove1(char name1[],char a);
 
-      Playermove ();
+void Playermove2(char name2[]);
+int main(void) {
+   call();
+   return 0;
+}
 
-      done = Result ();		/* see if winner */
+void call(void){
+    system("clear");
+    yellow();
+    printf("Do you want to play with another USER or COMPUTER?(u/c)");
+    char take;
+    scanf(" %c",&take);
+    if(take=='c'){
+        runC();
 
-      if (done != ' ')
-	break;			/* winner! */
-
-      Computermove ();
-
-      done = Result ();		/* see if winner */
     }
-  system ("clear");
-  yellow ();
-  if (done == 'X')
-    printf ("You won!!!!\n");
-  green ();
-  if (done == 'O')
-    {
-      printf ("Computer won!!!!\n");
-    }
-  if (done == 'd')
-    {
-      printf ("Draw!!!!\n");
-    }
-  reset ();
-  Show_matrix ();		/* show final positions */
+    else{
+        runU();
 
-  return 0;
+    }
+}
+void runU(void){
+    system("clear");
+        char done;
+        blue();
+        printf("GAME OF TIC TAC TOE:\n");
 
+        printf("PLAYER WILL BE PLAYING AGAINST USER...\n");
+        char name1[20];
+        char name2[20];
+        printf("Enter first player name:");
+        scanf("%s",name1);
+        printf("Enter second player name:");
+        scanf("%s",name2);
+        done = ' ';
+        Initializing();
+        system("clear");
+        printf("Using Lottery system for first move:\n");
+         srand(time(0));
+        int a=generate_random(0,1);
+        if(a==1){
+            yellow();
+            printf("Lottery won by %s first move by %s\n",name1,name1);
+            Show_matrix();
+           Playermove1(name1,'X'); 
+        }
+        else
+        {
+            yellow();
+            printf("Lottery won by %s first move by %s\n",name2,name2);
+            Show_matrix();
+            Playermove1(name2,'O');
+            
+        }
+        while (done == ' ') {
+
+                Show_matrix();
+                if(a==1)
+                Playermove1(name2,'O');
+                else Playermove1(name1,'X');
+                done = Result(); /* see if winner */
+
+                if (done != ' ')
+                        break; /* winner! */
+                Show_matrix();
+                  if(a==1)
+                Playermove1(name1,'X');
+                else Playermove1(name2,'O');
+
+                done = Result(); /* see if winner */
+        }
+        system("clear");
+        yellow();
+        if (done == 'X')
+            {   
+                printf("%s won!!!!\n",name1);  }
+        green();
+        if (done == 'O') {
+            {   
+                printf("%s won!!!!\n",name2); }
+        }
+        if (done == 'd') {
+            {
+                
+                printf("Draw!!!!\n"); }
+        }
+        reset();
+        Show_matrix();
+        /* show final positions */
+        check();
 }
 
+void runC(void) {
+    system("clear");
+        char done;
+        char name1[20];
+        blue();
+        printf("GAME OF TIC TAC TOE:\n");
+
+        printf("PLAYER WILL BE PLAYING AGAINST COMPUTER...\n");
+         printf("Enter Player name:");
+        scanf("%s",name1);
+        char name2[20]="COMPUTER";
+        done = ' ';
+        system("clear");
+        Initializing();
+        yellow();
+        printf("GAME OF TIC TAC TOE:\n");
+        printf("PLAYER WILL BE PLAYING AGAINST COMPUTER...\n");
+        printf("GAME CONTINUES:\n");
+        while (done == ' ') {
+
+                Show_matrix();
+
+                Playermove1(name1,'X');
+
+                done = Result(); /* see if winner */
+
+                if (done != ' ')
+                        break; /* winner! */
+
+                Computermove();
+
+                done = Result(); /* see if winner */
+        }
+        system("clear");
+        yellow();
+        if (done == 'X')
+                printf("%s won!!!!\n",name1);
+        green();
+        if (done == 'O') {
+                printf("Computer won!!!!\n");
+        }
+        if (done == 'd') {
+                printf("Draw!!!!\n");
+        }
+        reset();
+        Show_matrix();
+        /* show final positions */
+        check();
+}
+void check(void) {
+       
+        yellow();
+        printf("Do you want to start again?(y/n):");
+        char chh;
+        scanf(" %c", & chh);
+        if (chh == 'y') {
+                system("clear");
+                call();
+        }
+}
 /* Initialize the matrix. */
 
 void
-Initializing (void)
-{
+Initializing(void) {
 
-  int i, j;
+        int i, j;
 
-  for (i = 0; i < 3; i++)
+        for (i = 0; i < 3; i++)
 
-    for (j = 0; j < 3; j++)
-      matrix[i][j] = ' ';
+                for (j = 0; j < 3; j++)
+                        matrix[i][j] = ' ';
 
 }
 
-/* Get a player's move. */
-
+//get player move
 void
-Playermove (void)
-{
+Playermove1(char name1[],char a) {
 
-  int x, y;
-  green ();
-  printf ("Enter X,Y coordinates for your move: ");
-  scanf ("%d %d", &x, &y);
-  system ("clear");
-  x--;
-  y--;
-  yellow ();
-  printf ("GAME CONTINUES:\n");
-  if (matrix[x][y] != ' ')
-    {
-      Show_matrix ();
-      printf ("Invalid move, try again.\n");
+        int x, y, z;
+        green();
+        printf("Enter the cell in which you want to try your move %s: ",name1);
+        scanf("%d", & z);
+        system("clear");
+        yellow();
+        if (z == 1) {
+                x = 0;
+                y = 0;
+        } else if (z == 2) {
+                x = 0;
+                y = 1;
+        } else if (z == 3) {
+                x = 0;
+                y = 2;
+        } else if (z == 4) {
+                x = 1;
+                y = 0;
+        } else if (z == 5) {
+                x = 1;
+                y = 1;
+        } else if (z == 6) {
+                x = 1;
+                y = 2;
+        } else if (z == 7) {
+                x = 2;
+                y = 0;
+        } else if (z == 8) {
+                x = 2;
+                y = 1;
+        } else if (z == 9) {
+                x = 2;
+                y = 2;
+        }
+        printf("GAME CONTINUES:\n");
+        if (matrix[x][y] != ' ') {
+                Show_matrix();
+                printf("Invalid move, try again.\n");
 
-      Playermove ();
-    }
-  else
-    {
-      matrix[x][y] = 'X';
-    }
+                Playermove1(name1,a);
+        } else {
+                matrix[x][y] = a;
+        }
 }
 
 /* Get a move from the computer. */
-int generate_random(int l, int r) {     //this will generate random number in range l and r
-   int i;
-   int rand_num = (rand() % (r - l + 1)) + l;
-   return(rand_num);
+int generate_random(int l, int r) { //this will generate random number in range l and r
+        int i;
+        int rand_num = (rand() % (r - l + 1)) + l;
+        return (rand_num);
 }
 
 void
-Computermove (void)
-{
-   srand(time(0));
-  int i, j;
-    i=generate_random(0,2);
-    j=generate_random(0,2);
-    while(matrix[i][j] != ' '){
-	    i=generate_random(0,2);
-	    j=generate_random(0,2);
-    }
+Computermove(void) {
+        srand(time(0));
+        int i, j;
+        i = generate_random(0, 2);
+        j = generate_random(0, 2);
+        while (matrix[i][j] != ' ') {
+                i = generate_random(0, 2);
+                j = generate_random(0, 2);
+        }
 
-  matrix[i][j] = 'O';
+        matrix[i][j] = 'O';
 }
 
 /* Display the matrix on the screen. */
 
 void
-Show_matrix (void)
-{
+Show_matrix(void) {
 
-  int t;
-  for (t = 0; t < 3; t++)
-    {
-      green ();
-      printf ("\t\t\t %c | %c | %c ", matrix[t][0], matrix[t][1],
-	      matrix[t][2]);
-      yellow ();
-      if (t != 2)
-	printf ("\n\t\t\t---|---|---\n");
-    }
-  reset ();
-  printf ("\n");
+        int t;
+        for (t = 0; t < 3; t++) {
+                green();
+                printf("\t\t\t %c | %c | %c ", matrix[t][0], matrix[t][1],
+                        matrix[t][2]);
+                yellow();
+                if (t != 2)
+                        printf("\n\t\t\t---|---|---\n");
+        }
+        reset();
+        printf("\n");
 }
 
 /* See if there is a winner. */
 
 char
-Result (void)
-{
+Result(void) {
 
-  int i;
+        int i;
 
-  for (i = 0; i < 3; i++)	/* Result rows */
+        for (i = 0; i < 3; i++) /* Result rows */
 
-    if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2])
-      return matrix[i][0];
+                if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2])
+                        return matrix[i][0];
 
-  for (i = 0; i < 3; i++)	/* Result columns */
+        for (i = 0; i < 3; i++) /* Result columns */
 
-    if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i])
-      return matrix[0][i];
+                if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i])
+                        return matrix[0][i];
 
-/* test diagonals */
+        /* test diagonals */
 
-  if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2])
+        if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2])
 
-    return matrix[0][0];
+                return matrix[0][0];
 
-  if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0])
+        if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0])
 
-    return matrix[0][2];
-  int flag = 0;
-  for (int i = 0; i < 3; i++)
-    for (int j = 0; j < 3; j++)
-      {
-	if (matrix[i][j] == ' ')
-	  {
-	    flag = 1;
-	  }
-      }
-  if (flag == 0)
-    {
-      return 'd';
-    }
-  return ' ';
+                return matrix[0][2];
+        //for draw
+        int flag = 0;
+        for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++) {
+                        if (matrix[i][j] == ' ') {
+                                flag = 1;
+                        }
+                }
+        if (flag == 0) {
+                return 'd';
+        }
+        return ' ';
 
 }
